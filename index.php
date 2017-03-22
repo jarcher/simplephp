db36494dc0caeeef5512aa046db3374fc1e4199a
John Archer <jarcher@gmail.com>
2017-03-22 07:34:13 -0400

    cleaned

--- index.php
+++ index.php
@@ -2,8 +2,4 @@
 
 echo "<h1>Best app ever!!";
 
-echo "<br><br>Day Block is awesome";
 
-echo "<br><BR><BR><h1>This is how I code</h1>";
-echo "<br><BR><BR><h1>This is how I code</h1>";
-echo "<br><BR><BR><h1>This is how I code</h1>";
